02f586ad1b94e81e71eb419b6b7821d9caa7716c
Gabriel Horner <gabriel.horner@gmail.com>
2014-10-04 16:09:58 -0400

    Add command to attach to an existing id
    Also wrapped initial sharing in a command

--- src/lt/plugins/share.cljs
+++ src/lt/plugins/share.cljs
@@ -1,20 +1,50 @@
 (ns lt.plugins.share
   (:require [lt.objs.editor :as editor]
-            [lt.objs.editor.pool :as pool]))
+            [lt.objs.editor.pool :as pool]
+            [lt.objs.popup :as popup]
+            [lt.util.dom :as dom]
+            [lt.objs.platform :as platform]
+            [lt.objs.command :as cmd]
+            [lt.objs.notifos :as notifos]
+            [lt.objs.context :as ctx])
+  (:require-macros [lt.macros :refer [defui behavior]]))
 
-(def ed (pool/last-active))
+(defui text-input [m]
+  [:input {:type "text" :placeholder (:placeholder m)}]
+  :focus (fn []
+           (ctx/in! :popup.input))
+  :blur (fn []
+          (ctx/out! :popup.input)))
 
-(defn get-ref []
-  (let [ref (js/Firebase. "https://firepad.firebaseio-demo.com")
+(defn input [action-fn & {:as opts}]
+  (let [input (text-input opts)
+        p (popup/popup! {:header  (or (:header opts) "Enter value")
+                         :body input
+                         :buttons [{:label "Cancel"}
+                                   {:label "Submit"
+                                    :action (fn []
+                                              (action-fn (dom/val input)))}]})]
+    (dom/focus input)
+    (.setSelectionRange input 1000 1000)))
+
+
+;; Url used by older examples
+;; e.g. http://htmlpreview.github.io/?https://github.com/firebase/firepad/blob/master/examples/code.html
+;; (def firepad-url "https://firepad.firebaseio-demo.com")
+;; Url used by http://demo.firepad.io/
+(def firepad-url "https://firepad.firebaseio.com/demo")
+
+(defn share-ref []
+  (let [ref (js/Firebase. firepad-url)
         ref (.push ref)]
     ref))
 
-(def ref (get-ref))
+(defn attach-ref [id]
+  (let [ref (js/Firebase. firepad-url)
+        ref (.child ref id)]
+    ref))
 
-(.name ref)
-
-(defn attach [ed ref]
-  #_(editor/select-all ed)
+(defn attach-cm [ed ref]
   (editor/set-selection ed
                  {:line 0 :ch 0}
                  {:line (editor/last-line ed)})
@@ -23,4 +53,22 @@
     (js/Firepad.fromCodeMirror ref (editor/->cm-ed ed))
     (editor/replace-selection ed text)))
 
-(attach ed ref)
+(cmd/command {:command :share.attach
+              :desc "Share: Attaches current tab to given ID"
+              :exec (fn []
+                      (let [ed (pool/last-active)
+                            input-callback (fn [ed input]
+                                             (attach-cm ed (attach-ref input))
+                                             (notifos/set-msg! (str "Attached to " (pr-str input))))
+                            ]
+                        (input (partial input-callback ed) :header "Enter ID")))})
+
+(cmd/command {:command :share.share
+              :desc "Share: Shares current tab and copies id to clipboard"
+              :exec (fn []
+                      (let [ed (pool/last-active)
+                            ref (share-ref)
+                            id (.name ref)]
+                        (attach-cm ed ref)
+                        (platform/copy id)
+                        (notifos/set-msg! (str "Shared to " (pr-str id)))))})
